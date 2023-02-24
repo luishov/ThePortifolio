397451d02fc98d7b409b7cd0de88c3650e3fdc2f
luishov <luishovieira@yahoo.com.br>
2023-02-24 19:36:24 -0300

    adicionei dashboard e Navbar

--- portifolio/resources/views/components/dashboard/signature-modal.blade.php
+++ portifolio/resources/views/components/dashboard/signature-modal.blade.php
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-<div>
-      <!-- Modal -->
-<div class="modal fade" id="modalSignature" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modalSignature" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalSignatureLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-=======
  <!-- Modal -->
  <div class="modal fade" id="modalSignature" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modalSignatureLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -42,4 +18,3 @@
       </div>
     </div>
   </div>
->>>>>>> 479111d42c3657a35489eba41a32bebde7ab6f76
